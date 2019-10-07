a7a908fc589157b7052bdf33b3c902f1fa031810
Maria Ratino <mlratino@gmail.com>
2019-10-06 22:48:45 -0400

    Giphy Homework for credit.
    minimum requirements.  I am pretty excited though.  It didn't take me as long to get this far.  I have a tutoring session Monday.  Will submit chunks of work and attend office hours and 2 or 3 tutoring sessions.  Should be caught up this week.

---
+++ #README.txt
@@ -0,0 +1,23 @@
+In this assignment, I use the GIPHY API to make a dynamic web page that 
+populates with gifs of your choice. 
+To finish this task, you must call the GIPHY API and use JavaScript and jQuery 
+to change the HTML of your site.
+
+Like many APIs, GIPHY requires developers to use a key to access their API data. 
+Obtained an API Key by creating an app.
+Make sure you switch the protocol in the query URL from http to https,
+or the app may not work properly when deployed to Github Pages.
+
+When the user clicks on a button, the page should grab 10 static, 
+non-animated gif images from the GIPHY API and place them on the page.
+When the user clicks one of the still GIPHY images, the gif should animate. 
+If the user clicks the gif again, it should stop playing.
+
+Display its rating (PG, G, so on).
+
+This data is provided by the GIPHY API.
+
+a form that takes a value from a user input box and adds 
+it to the topics array. Then a function call takes each topic 
+in the array and remakes the buttons on the page.
+
---
+++ app.js
@@ -0,0 +1,97 @@
+$(document).ready(function() {
+
+  var characters = [
+    "Michael Scott", "Dwight Schrutte", "Jim Halpert", "Pam Beesly", "Angela Martin", "Kevin Malone",
+    "Darryl Philbin", "Erin Hannon", "Holly Flax", "Meredith Palmer", "Ryan Howard","Clark Green",
+    "Andy Bernard", "Robert California", "Jan Levinson", "Kelly Kapoor", "Toby Flenderson",
+    "Stanley Hudson", "Oscar Martinez", "Phyllis Vance", "Creed Bratton"
+  ];
+
+  // This functionwill make the add buttons and display need to move the button and figure out how to handle the topic control. You can write anything in there************************************
+//   What if I wanted to limit the input in any way?
+  
+    function populateButtons(arrayToUse, classToAdd, areaToAddTo) {
+    $(areaToAddTo).empty();
+
+    for (var i = 0; i < arrayToUse.length; i++) {
+      var a = $("<button>");
+      a.addClass(classToAdd);
+      a.attr("data-type", arrayToUse[i]);
+      a.text(arrayToUse[i]);
+      $(areaToAddTo).append(a);
+    }
+
+  }
+// create the on-click*****************************************************
+  $(document).on("click",".character-button",function() {
+    $("#characters").empty();
+    $(".character-button").removeClass("active");
+    $(this).addClass("active");
+
+    var type = $(this).attr("data-type");
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=t9jqJfTkXGrRPkurtPfCntbdF38gfq33&limit=11";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      .then(function(response) 
+      {
+        var results = response.data;
+
+        for (var i = 0; i < results.length; i++) 
+        {
+          var characterDiv = $("<div class=\"character-item\">");
+
+          var rating = results[i].rating;
+
+          var p = $("<p>").text("Rating: " + rating);
+
+          var animated = results[i].images.fixed_height.url;
+          var still = results[i].images.fixed_height_still.url;
+
+          var characterImage = $("<img src=" + still + ">");
+          characterImage.attr("src", still);
+          characterImage.attr("data-still", still);
+          characterImage.attr("data-animate", animated);
+          characterImage.attr("data-state", "still");
+          characterImage.addClass("character-image");
+
+          characterDiv.append(p);
+          characterDiv.append(characterImage);
+
+          $("#characters").append(characterDiv);
+        }
+      });
+  });
+
+  $(document).on("click",".character-image",function() 
+  {
+
+    var state = $(this).attr("data-state");
+
+    if (state === "still") {
+      $(this).attr("src", $(this).attr("data-animate"));
+      $(this).attr("data-state", "animate");
+    }
+    else {
+      $(this).attr("src", $(this).attr("data-still"));
+      $(this).attr("data-state", "still");
+    }
+  });
+console.log("Mose is the best");
+  $("#add-character").on("click", function(event) {
+    event.preventDefault();
+    var newCharacter = $("input").eq(0).val();
+
+    if (newCharacter.length > 2) {
+      characters.push(newCharacter);
+    }
+
+    populateButtons(characters, "character-button", "#character-buttons");
+
+  });
+
+  populateButtons(characters, "character-button", "#character-buttons");
+});
+console.log("That's what she said!");
